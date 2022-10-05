40625b2b7aa0eb50d276142b0b87194a6896ab46
jgrmy <julien.roguet@epitech.eu>
2022-10-05 18:23:47 +0200

    TEK-test

---
+++ DAY03/my_isneg.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** isneg
+** File description:
+** isneg
+*/
+
+#include <unistd.h>
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int my_isneg(int n)
+{
+    if (n < 0) {
+        my_putchar('N');
+    } else {
+        my_putchar('P');
+    }
+    return (0);
+}
