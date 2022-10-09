500b92aeff41a21685ce49e7567d78fc5fdc09e6
jgrmy <julien.roguet@epitech.eu>
2022-10-09 17:51:44 +0200

    star up starting down

--- STAR/star.c
+++ STAR/star.c
@@ -13,6 +13,8 @@ void my_putchar(char c);
 void star(int x);
 void start(int xt);
 void hat(int xh);
+void line_up(int xl);
+void branches(int xb);
 
 void my_putchar(char c)
 {
@@ -43,29 +45,78 @@ void star(int x)
 
 void start(int xt) {
         int i;
-        i = ((xt * 2) + 1) - 1;
+        int xh;
+        i = (xt * 2) + xt;
         while(i != 0) {
             my_putchar(space);
             i--;
             }
         my_putchar(stars);
         my_putchar(retour);
-        hat(xt)
+        xh = xt - 1;
+        hat(xt);
 }
 
 
 void hat(int xh){
-        i = ((xh * 2) + 1) - 2;
-        while(i != 0) {
-            my_putchar(space);
-            i--;
-        }
+        int l = 1;
+        while(l != xh){
+            int i;
+            i = ((xh * 2) + 1) - 1 - l + xh;
+            while(i != 0) {
+                my_putchar(space);
+                i--;
+            }
+            my_putchar(stars);
+            i = (l * 2) - 1;
+            while(i != 0) {
+                my_putchar(space);
+                i--;
+            }
         my_putchar(stars);
-        i = xh;
-        while(i != 0) {
+        my_putchar(retour);
+        l++;
+            }
+        line_up(xh);
+        }
+
+
+void line_up(int xl)
+{
+    int c = (xl * 2) + 1;
+    while(c != 0) {
+        my_putchar(stars);
+        c--;
+    }
+    int d = (xl * 2) - 1;
+    while(d != 0)
+    {
+        my_putchar(space);
+        d--;
+    }
+    int second_e = (xl * 2) + 1;
+    while(second_e != 0) {
+        my_putchar(stars);
+        second_e--;
+    }
+my_putchar(retour);
+branches(xl);
+}
+
+void branches(int xb) 
+{
+    my_putchar(retour);
+    int l = xb;
+    int i = xb * 3;
+    while(l != 0){
+        while(i != 0){
             my_putchar(space);
             i--;
         }
         my_putchar(stars);
         my_putchar(retour);
+        
+    l--;
+    }
+    my_putchar(stars);
 }
