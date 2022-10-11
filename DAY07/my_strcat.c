9a68ed280f6bf8ca1101fe43db7c323d04e04d14
juliengremy <juliengremy@penguin>
2022-10-11 17:29:14 +0200

    my_strcat

--- DAY07/my_strcat.c
+++ DAY07/my_strcat.c
@@ -1,1 +1,20 @@
-test
+/*
+** EPITECH PROJECT, 2022
+** my_strcat
+** File description:
+** my_strcat
+*/
+
+int my_strlen(char const *str);
+
+char *my_strcat(char *dest, char const *src)
+{
+    int to_concatenate_len = my_strlen(src);
+    int concatenated_len = my_strlen(dest);
+
+    for (int i = 0; i < to_concatenate_len; i++) {
+        int index = concatenated_len + i;
+        dest[index] = src[i];
+    }
+    return (dest);
+}
