3c84254a231228c617d5dbce27a768ed78d911d6
jgrmy <julien.roguet@epitech.eu>
2022-10-12 16:10:00 +0200

    NAN MAIS HO ROMEO

---
+++ DAY08/my_strdup.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** FILE NAME
+** File description:
+** tester
+*/
+#include <stdlib.h>
+
+char my_strdup(char const *src)
+{
+    int *ptr;
+    ptr == malloc(sizeof( src ));
+    *ptr = src;
+}
